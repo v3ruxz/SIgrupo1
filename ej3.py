db11fb0d853bf920945e9e02aa93ee53c9c42930
Boni <b.bonilla.2020@alumnos.urjc.es>
2024-03-17 20:59:50 +0100

    ej 3 juntar codigos, permisos y contraseñas

--- ej3.py
+++ ej3.py
@@ -1,8 +1,11 @@
 import sqlite3
 import pandas as pd
 import statistics
+import hashlib
 
-'''
+
+'''  
+Valores pedidos del ejercicio 3
 •	Numero de observaciones
 •	Numero de valores ausentes (missing)
 •	Mediana
@@ -13,6 +16,20 @@ RockYou 15 - MD5
 https://github.com/danielmiessler/SecLists/blob/master/Passwords/Leaked-Databases/rockyou-15.txt
 '''
 
+# Función para cargar el diccionario SmallRockYou
+def cargar_diccionario():
+    with open('rockyou-15.txt', 'r', encoding='latin-1') as f:
+        return [line.strip() for line in f]
+
+# Función para descifrar una contraseña cifrada con MD5
+def descifrar_md5(hash):
+    with open('rockyou-15.txt', 'r', encoding='latin-1') as f:
+        for line in f:
+            password = line.strip()
+            if hashlib.md5(password.encode()).hexdigest() == hash:
+                return password
+    return None
+
 def calcular_datos(df):
     print("Numero de observaciones: " + str(sum(df['email_phishing'])))
     print("Numero de valores ausentes: " + str((df['email_phishing'].isnull().sum())))
@@ -22,6 +39,7 @@ def calcular_datos(df):
     print("Valor maximo: " + str(df['email_phishing'].max()))
     print("Valor minimo: " + str(df['email_phishing'].min()))
 
+
 if __name__ == '__main__':
 
     # Conexión a la base de datos
@@ -29,12 +47,36 @@ if __name__ == '__main__':
     query = '''SELECT * FROM usuarios'''
     df_base = pd.read_sql_query(query, conn)
 
+    # Identificación de contraseñas débiles
+    diccionario_rockyou = cargar_diccionario()
+    query_contraseñas = "SELECT username, contrasena FROM usuarios"
+    df_contraseñas = pd.read_sql_query(query_contraseñas, conn)
+
+    contraseñas_debiles = []
+    contraseñas_no_debiles = []
+    for index, row in df_contraseñas.iterrows():
+        contraseña_cifrada = descifrar_md5(row['contrasena'])
+        if contraseña_cifrada in diccionario_rockyou:
+            contraseñas_debiles.append(row['username'])
+        else:
+            contraseñas_no_debiles.append(row['username'])
+
+    # DataFrames
     df_users0 = df_base.loc[df_base.permisos == 0]
     df_users1 = df_base.loc[df_base.permisos == 1]
-    # df_usersWeak =
-    # df_usersNotWeak =
+
+    query_contraseñas_debiles = f"SELECT * FROM usuarios WHERE username IN ({','.join(['?'] * len(contraseñas_debiles))})"
+    df_usersWeak = pd.read_sql_query(query_contraseñas_debiles, conn, params=contraseñas_debiles)
+
+    query_contraseñas_no_debiles = f"SELECT * FROM usuarios WHERE username IN ({','.join(['?'] * len(contraseñas_no_debiles))})"
+    df_usersNotWeak = pd.read_sql_query(query_contraseñas_no_debiles, conn, params=contraseñas_no_debiles)
+
     print(df_base)
     print("\n -Usuarios permisos 0:")
     calcular_datos(df_users0)
     print("\n -Usuarios permisos 1:")
     calcular_datos(df_users1)
+    print("\n -Usuarios con contraseñas débiles:")
+    calcular_datos(df_usersWeak)
+    print("\n -Usuarios con contraseñas no débiles:")
+    calcular_datos(df_usersNotWeak)
